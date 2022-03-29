91fd76c0e5d705b592b0e2770bdf6a7705f15311
Mohammed Olaiya <mohammedolaiya@gmail.com>
2022-03-28 21:55:02 -0400

    Create README.r

---
+++ README.r
@@ -0,0 +1,15 @@
+# r-excercise-5
+This is another excercise from codecademy
+
+
+library(readr)
+library(dplyr)
+
+
+sales <- read_csv("sales.csv")
+targets <- read_csv("targets.csv")
+
+
+
+head(sales)
+head(targets)
