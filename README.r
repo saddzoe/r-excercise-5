134f74f4f7bea4736d6e5182e35f5a2a71f894f9
Mohammed Olaiya <mohammedolaiya@gmail.com>
2022-03-31 21:25:10 -0400

    Update README.r

--- README.r
+++ README.r
@@ -13,3 +13,22 @@ targets <- read_csv("targets.csv")
 
 head(sales)
 head(targets)
+
+
+```{r}
+# define sales_vs_targets here:
+sales_vs_targets <- sales %>%
+  inner_join(targets)
+
+sales_vs_targets
+```
+
+```{r}
+# define crushing_it here:
+crushing_it <- sales_vs_targets %>%
+  filter(revenue > target)
+
+crushing_it
+
+
+```
